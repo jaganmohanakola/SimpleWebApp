0e6864a4d7d71b8a3433de1ee5a0453381e089f9
jaganmohanakola <50134823+jaganmohanakola@users.noreply.github.com>
2020-05-15 23:13:08 +0530

    Update ShellScripts.sh

--- ShellScripts.sh
+++ ShellScripts.sh
@@ -14,7 +14,7 @@ stopTomcat()
  if [ -f /opt/tomcat/bin/shutdown.sh ]
  then
  echo " file exists please start"
- chmod 777 -R /opt/tomcat/
+ # chmod 777 -R /opt/tomcat/
  sh /opt/tomcat/bin/shutdown.sh
  echo "shutdown script running"
  sleep 20
@@ -34,7 +34,7 @@ startTomcat()
  echo "tomcat is already running"
  else
  echo "tomcat is not running going to start"
- chmod 777 -R /opt/tomcat/
+ # chmod 777 -R /opt/tomcat/
  sh /opt/tomcat/bin/startup.sh
  echo "tomcat start up"
  sleep 20
