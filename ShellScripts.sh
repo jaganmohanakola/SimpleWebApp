848761f095751ab8c109d26dc2b2b77fbb0cf395
jaganmohanakola <50134823+jaganmohanakola@users.noreply.github.com>
2020-05-16 14:09:55 +0530

    Update ShellScripts.sh

--- ShellScripts.sh
+++ ShellScripts.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-export MAVEN_HOME=/opt/maven
-export PATH=$PATH:$MAVEN_HOME/bin
-
 stopTomcat()
 {
  if netstat -lntp | grep 8085
@@ -14,7 +10,6 @@ stopTomcat()
  if [ -f /opt/tomcat/bin/shutdown.sh ]
  then
  echo " file exists please start"
- # chmod 777 -R /opt/tomcat/
  sh /opt/tomcat/bin/shutdown.sh
  echo "shutdown script running"
  sleep 20
@@ -34,7 +29,6 @@ startTomcat()
  echo "tomcat is already running"
  else
  echo "tomcat is not running going to start"
- # chmod 777 -R /opt/tomcat/
  sh /opt/tomcat/bin/startup.sh
  echo "tomcat start up"
  sleep 20
@@ -44,11 +38,10 @@ startTomcat()
 appBackUp()
 {
  echo "inside app Back up func"
- sleep 10
- if [ -f /opt/tomcat/webapps/simpleWebApp.war ]
+ if [ -f /opt/tomcat/webapps/*.war ]
  then
  echo "file exists so please take backup"
- cp /opt/tomcat/webapps/*.war /root/myBackUp/
+ cp /opt/tomcat/webapps/*.war ./myBackUp/
  echo "file moved to back up diecctory"
  else
  echo "fnf so can not take back up"
@@ -60,11 +53,21 @@ appDeploy()
  startTomcat
  appBackUp
  echo "strat deploying the war file"
- if [ -f /var/lib/jenkins/workspace/MySimpleWebApp/pom.xml ]
+ # if [ -f /root/myRepository/SimpleWebApp/pom.xml ]
+  if [ -f /root/myRepository/MyEcomm/pom.xml ]
  then
  echo "pom file exists and ready to deploy"
- cd /var/lib/jenkins/workspace/MySimpleWebApp
- mvn clean tomcat7:deploy
+ cd /root/myRepository/SimpleWebApp
+ # cd /root/myRepository/MyEcomm
+ 
+ if [ -f /opt/tomcat/webapps/SimpleWebApp.war ]
+ then
+ echo "going to redeploy"
+ mvn tomcat7:redeploy
+ else
+ echo "going to deploy"
+ mvn tomcat7:deploy
+ fi
  else
  echo "pom file does not exists please check"
  fi
@@ -75,13 +78,18 @@ appUnDeploy()
  if netstat -lntp | grep 8085
  then
  echo "tomcat is running, can do undeploy"
- if [ -f /var/lib/jenkins/workspace/MySimpleWebApp/pom.xml ]
+ if [ -f /root/myRepository/SimpleWebApp/pom.xml ]
+ # if [ -f /root/myRepository/MyEcomm/pom.xml ]
  then
+ 
  echo "pom file exists doing undeploy by going to the path"
- if [ -f /opt/tomcat/webapps/simpleWebApp.war ]
+ # if [ -f /opt/tomcat/webapps/myEcommercePath.war ]
+ 
+ if [ -f /opt/tomcat/webapps/SimpleWebApp.war ]
  then 
   echo "war file exists so you can start undeploy"
- cd  /var/lib/jenkins/workspace/MySimpleWebApp/
+  cd  /root/myRepository/SimpleWebApp/
+ # cd /root/myRepository/MyEcomm
  echo "going to undeploy"
  mvn tomcat7:undeploy
  else
@@ -100,10 +108,16 @@ appReDeploy()
  if netstat -lntp | grep 8085
  then
  echo "tomcat is running"
- if [ -f /var/lib/jenkins/workspace/MySimpleWebApp/pom.xml ]
+ # if [ -f /root/myRepository/SimpleWebApp/pom.xml ]
+ if [ -f /root/myRepository/MyEcomm/pom.xml ]
+ 
+ #  if [ -f /root/myRepository/PetClinic/pom.xml ]
  then
  echo "pom file exists going to the path directory and doing redeploy"
- cd /var/lib/jenkins/workspace/MySimpleWebApp/
+ # cd /root/myRepository/SimpleWebApp
+ cd /root/myRepository/MyEcomm
+ 
+ # cd /root/myRepository/PetClinic
  echo "going torun command  redeploy"
  mvn tomcat7:redeploy
  echo "redeploy command executed"
